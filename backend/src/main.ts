db2feaf1ecba92011ab0000291ab82b3df47cbde
AlexeiEvenepoel <e_2019200797H@uncp.edu.pe>
2025-09-08 12:30:54 -0500

    cors 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://expressv2-production.up.railway.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-app.enableCors({
-  origin: 'https://expressv2-production.up.railway.app',
-});
+
+// app.enableCors({
+//   origin: [
+//     'http://localhost:4200',
+//     'https://panel-ecommerce.netlify.app',
+//     'https://www.synerbyteperu.com', // <--- asegúrate que este esté incluido
+//     'https://synerbyteperu.com',
+//     'https://www.synerbyteperu.com/', // <--- puedes dejar ambos
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//   credentials: true,
+// });
